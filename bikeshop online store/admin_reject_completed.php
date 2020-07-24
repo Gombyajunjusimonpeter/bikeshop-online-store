143172b7aabe3c6cf07abc7dc18c2f7fee9b8597
cymon6192 <49641137+cymon6192@users.noreply.github.com>
2020-07-24 15:45:07 +0300

    Add files via upload

---
+++ bikeshop online store/admin_reject_completed.php
@@ -0,0 +1,8 @@
+
+<?php
+    include('includes/dbconn.php');
+   $id = $_GET['id'];
+   $sql = ("DELETE FROM tblorders where id = '$id'") or die (mysqli_error());
+   $result=mysqli_query($con, $sql);
+   header("location:index.php");
+?>
